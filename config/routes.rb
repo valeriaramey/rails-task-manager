41075a114889b1b61a6a4dbcab8e3fc0628e1e42
Valeria Ramey <valeriaramey@gmail.com>
2020-08-11 13:33:27 -0400

    edit task complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # index in the browser to list all tasks
+  get 'tasks', to: 'tasks#index'
+  # display NEW task form in the browser
+  get 'tasks/new', to: 'tasks#new'
+  # Create a new task i nthe serrver
+  post 'tasks', to: 'tasks#create'
+  # Edit FORM to update the task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # UPDATE in the server
+  patch 'tasks/:id', to: 'tasks#update'
+  # to read one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
